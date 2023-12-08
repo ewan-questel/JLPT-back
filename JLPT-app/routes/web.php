75e862ac4d0e9b757111fe737c9d965a0b9678a8
ewan questel <ew.questel@gmail.com>
2023-12-08 11:41:35 +0100

    Create back
    Sets up Laravel and the necessary measures to create the basics of the database "jlpt-ddb" (with the default laravel migrations)

---
+++ JLPT-app/routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/auth/redirect', function () {
+    return Socialite::driver('google')->redirect();
+});
+ 
+Route::get('/auth/callback', function () {
+    $userProvider = Socialite::driver('google')->user();
+    $user = User::updateOrCreate(
+        [
+            'email'=> $userProvider->email
+        ],[
+            'name'=> $userProvider->name,
+            'color'=> null
+        ]
+    );
+    // Faire quelque chose avec les données utilisateur ($user)
+    $token = $user->createToken('Api token');
+
+    return response()->json([
+        'token'=> $token,
+        'user' => $user,
+    ]);
+    // $user->token
+});
