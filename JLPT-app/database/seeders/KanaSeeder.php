1ac26ab2a2bacf038816e28403ff8010180c50f6
ewan questel <ew.questel@gmail.com>
2023-12-18 12:29:48 +0100

    Modified Kana seed
    Added a JSON file for the kana to simplify the seeding and make lisibility better

--- JLPT-app/database/seeders/KanaSeeder.php
+++ JLPT-app/database/seeders/KanaSeeder.php
@@ -16,37 +16,19 @@ class KanaSeeder extends Seeder
      */
     public function run()
     {
-        // Define hiragana and katakana characters along with their pronunciations
-        $kanaData = [
-            // Add hiragana
-            'あ' => 'a', 'い' => 'i', 'う' => 'u', 'え' => 'e', 'お' => 'o',
-            'か' => 'ka', 'き' => 'ki', 'く' => 'ku', 'け' => 'ke', 'こ' => 'ko',
-            'さ' => 'sa', 'し' => 'shi', 'す' => 'su', 'せ' => 'se', 'そ' => 'so',
-            'た' => 'ta', 'ち' => 'chi', 'つ' => 'tsu', 'て' => 'te', 'と' => 'to',
-            'な' => 'na', 'に' => 'ni', 'ぬ' => 'nu', 'ね' => 'ne', 'の' => 'no',
-            'は' => 'ha', 'ひ' => 'hi', 'ふ' => 'fu', 'へ' => 'he', 'ほ' => 'ho',
-            'ま' => 'ma', 'み' => 'mi', 'む' => 'mu', 'め' => 'me', 'も' => 'mo',
-            'や' => 'ya', 'ゆ' => 'yu', 'よ' => 'yo',
-            'ら' => 'ra', 'り' => 'ri', 'る' => 'ru', 'れ' => 're', 'ろ' => 'ro',
-            'わ' => 'wa', 'を' => 'wo',
-            'ん' => 'n',
-            
-            // Add katakana
-            'ア' => 'a', 'イ' => 'i', 'ウ' => 'u', 'エ' => 'e', 'オ' => 'o',
-            'カ' => 'ka', 'キ' => 'ki', 'ク' => 'ku', 'ケ' => 'ke', 'コ' => 'ko',
-            'サ' => 'sa', 'シ' => 'shi', 'ス' => 'su', 'セ' => 'se', 'ソ' => 'so',
-            'タ' => 'ta', 'チ' => 'chi', 'ツ' => 'tsu', 'テ' => 'te', 'ト' => 'to',
-            'ナ' => 'na', 'ニ' => 'ni', 'ヌ' => 'nu', 'ネ' => 'ne', 'ノ' => 'no',
-            'ハ' => 'ha', 'ヒ' => 'hi', 'フ' => 'fu', 'ヘ' => 'he', 'ホ' => 'ho',
-            'マ' => 'ma', 'ミ' => 'mi', 'ム' => 'mu', 'メ' => 'me', 'モ' => 'mo',
-            'ヤ' => 'ya', 'ユ' => 'yu', 'ヨ' => 'yo',
-            'ラ' => 'ra', 'リ' => 'ri', 'ル' => 'ru', 'レ' => 're', 'ロ' => 'ro',
-            'ワ' => 'wa', 'ヲ' => 'wo',
-            'ン' => 'n',
-        ];
+        // Read kana data from JSON file
+        $jsonFilePath = database_path('json/kana.json');
+        $kanaData = json_decode(file_get_contents($jsonFilePath), true);
 
         // Insert data into the kana table
-        foreach ($kanaData as $character => $pronunciation) {
+        foreach ($kanaData['hiragana'] as $character => $pronunciation) {
+            Kana::create([
+                'character' => $character,
+                'pronunciation' => $pronunciation,
+            ]);
+        }
+
+        foreach ($kanaData['katakana'] as $character => $pronunciation) {
             Kana::create([
                 'character' => $character,
                 'pronunciation' => $pronunciation,
